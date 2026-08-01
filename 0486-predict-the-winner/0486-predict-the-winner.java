bbc63f2ec9447eab4f9bbbd00df1045514ffd033
Aayush Shelar <ayushshelar20@gmail.com>
2026-08-01 22:54:55 +0530

    Time: 76 ms (12.75%), Space: 42.7 MB (74.96%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solve(int []nums,int i,int j,Integer[][] dp){
+
+        if(i==j)return nums[i];
+
+        if(dp[i][j]!=null)return dp[i][j];
+
+        int left=nums[i]-solve(nums,i+1,j,dp);
+        int right=nums[j]-solve(nums,i,j-1,dp);
+
+        return Math.max(left,right);
+    }
+    public boolean predictTheWinner(int[] nums) {
+        
+        int n=nums.length;
+
+        Integer dp[][]=new Integer[n][n];
+
+        return solve(nums,0,n-1,dp)>=0;
+    }
+}
